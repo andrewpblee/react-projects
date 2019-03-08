db7dc7b8a2460c715beefd75f2b960d778267816
AndrewLeeNOTHS <andrewlee@notonthehighstreet.com>
2019-03-08 12:54:03 +0000

    initial commit

---
+++ jsx/src/index.js
@@ -0,0 +1,24 @@
+// import react and react dom libraries
+import React from "react"; 
+import ReactDOM from "react-dom";
+
+const App = () => {
+    const buttonText = {text: 'Click Me!'};
+
+    return (
+        <div>
+            <label className= "label" for="name">Enter Name:</label>
+            <input id="name" type="text"/>
+            <button style= {{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
+        </div>
+    );
+};
+// create a react component 
+
+
+
+// Take the react component and display
+ReactDOM.render(
+    <App />, 
+    document.querySelector("#root")
+);

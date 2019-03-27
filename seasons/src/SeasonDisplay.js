d2251a7ffc91e88f1533ed13e05d76ea470aedd7
AndrewLeeNOTHS <andrewlee@notonthehighstreet.com>
2019-03-27 11:06:31 +0000

    new pics app

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,41 +1,37 @@
-import React from 'react';
+import React from "react";
+import "./SeasonDisplay.css";
 
 const seasonConfig = {
-    summer: {
-        text: "Let's hit the beach", 
-        iconName: "sun"
-    },
-    winter: {
-        text: "It's so cold!", 
-        iconName: "snowflake"        
-    }
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "It's so cold!",
+    iconName: "snowflake"
+  }
 };
 
-
-const getSeason = (lat,month) => {
-    if (month > 2 && month < 9) {
-       return lat > 0 ? 'summer': 'winter';
-    } 
-
-    else {
-        return lat < 0 ? 'summer': 'winter';
-    }
-}
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "summer" : "winter";
+  }
+};
 
 const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
 
-    const season = getSeason(props.lat, new Date().getMonth());
-    const {text, iconName} = seasonConfig[season]
+  console.log(season);
 
-    console.log(season);
-
-    return (
-        <div>
-            <i className= {`massive ${iconName} icon`} />
-            <h1>{text}</h1>
-            <i className= {`massive ${iconName} icon`} />
-        </div>
-    );
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} /> <h1> {text} </h1>{" "}
+      <i className={`icon-right massive ${iconName} icon`} />{" "}
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-    state = {lat: null, errorMessage:''};
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({lat: position.coords.latitude}), 
-            err => this.setState({errorMessage: err.message})
-        );
-    }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position =>
+        this.setState({
+          lat: position.coords.latitude
+        }),
+      err =>
+        this.setState({
+          errorMessage: err.message
+        })
+    );
+  }
 
-    render() {
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+        return <div> Error: {this.state.errorMessage} </div>;
+      }
+      if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />;
+      }
+      return <Spinner message='help!' />;
+  }
 
-        return (
-            <SeasonDisplay lat = {this.state.lat} />
-        );
-    }
+  render() {
+    return (
+        <div>{this.renderContent()}</div>
+    )
+  }
 }
 
-ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));

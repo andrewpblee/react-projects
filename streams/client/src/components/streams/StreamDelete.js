3360a00c7acaac42f22ac7a442715569ce9e0efd
AndrewLeeNOTHS <andrewlee@notonthehighstreet.com>
2019-07-17 14:12:53 +0100

    17-07-2019

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import Modal from "../Modal";
+import React from "react";
 
 const StreamDelete = () => {
-    return <div>StreamDelete</div>
+  const action = (
+    <div>
+      <button className="ui primary negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </div>
+  );
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
